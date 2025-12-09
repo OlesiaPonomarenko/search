559e1604e660a673ee085e04b1cb1a092c241688
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-12-09 23:03:21 +0200

    new

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const searchInput = document.querySelector('#search');
+const movies = document.querySelectorAll('.movie');
+const picture = document.querySelectorAll('.faceHeader');
+
+searchInput.addEventListener('keyup', (event) => {
+
+    const word = event.target.value.toLowerCase();
+    movies.forEach(item => {
+        item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display='block') : (item.style.display='none');
+    })
+})
+
+picture.forEach( item =>{
+   item.addEventListener('mouseover', () => {
+    deleteclass();
+    item.classList.add('new');
+})
+deleteclass = () => {
+    picture.forEach( item => {
+    item.classList.remove('new');
+    })
+}
+})
